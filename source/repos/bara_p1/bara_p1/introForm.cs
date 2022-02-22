d3ebb68e2edb134738356feb0e55cdc125f0ecfb
songhyo <thdgyqls1110@naver.com>
2022-02-22 16:28:57 +0900

    bara_p1

---
+++ source/repos/bara_p1/bara_p1/introForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bara_p1
+{
+    public partial class introForm : Form
+    {
+        public introForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ source/repos/bara_p1/bara_p1/sale_point_Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bara_p1
+{
+    public partial class sale_point_Form1 : Form
+    {
+        public sale_point_Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
